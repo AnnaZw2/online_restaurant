5814bf0fbf201669e9e28b4f97929b9df87f9d11
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-10-14 17:40:55 +0200

    init zad_04

---
+++ lab_02/src/zad_04/Book.ts
@@ -0,0 +1,11 @@
+class Book {
+    private  title: string;
+    private author: string;
+
+  
+  public  constructor( title: string, author: string) {
+      this.title = title;
+      this.author = author;
+      
+    }
+  }
---
+++ lab_02/src/zad_04/enums/BookStatus.ts
@@ -0,0 +1,6 @@
+export enum BookStatus {
+    RENTED = "RENTED",
+    LOST = "LOST",
+    AVAIBLE = "AVAIBLE"
+}
+
