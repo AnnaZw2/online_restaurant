ed85b7e34587ddd480ffef6017eb489269e257c5
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-10-17 15:00:36 +0200

    finished zad_01

--- lab_03/zad_01/src/app/app.module.ts
+++ lab_03/zad_01/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FooterComponent } from './footer/footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
 import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    FooterComponent,
+    NavigationComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
