03a25033967fe959fe0e5c9db86df52ddf24303f
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2023-10-24 15:00:09 +0200

    basic counter implementation

---
+++ lab_04/zad_01/src/app/counter/counter.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.sass']
+})
+export class CounterComponent {
+public counter = 0
+public interval: any;
+  increment(){
+   this.counter++;
+  }
+  decrement(){
+    this.counter--;
+  }
+
+  reset(){
+    this.counter= 0
+  }
+
+  start(){
+   this.interval= setInterval(() => {
+      this.increment();
+    }, 1000);
+  }
+
+  stop(){
+    clearInterval(this.interval)
+
+  }
+
+
+}
