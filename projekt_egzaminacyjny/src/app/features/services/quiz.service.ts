dd3753060b4f0481e0e8c97d29202b38439a4b30
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2024-01-05 14:24:09 +0100

    use const for backend url

--- projekt_egzaminacyjny/src/app/features/services/quiz.service.ts
+++ projekt_egzaminacyjny/src/app/features/services/quiz.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { API_URL } from 'src/app/core/constants/api_url';
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
-  // private api_url = 'http://localhost:3000/';
+  private quiz_api_url = API_URL + "/quizzes";
   constructor(private http: HttpClient) {}
+
+  getQuzzes() {
+    return this.http.get(this.quiz_api_url);
+  }
 }
