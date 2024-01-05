c843c54736a2d1e5e3e6a2adea32e79499de6ac7
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2024-01-05 16:43:33 +0100

    add redirect to home when clicking on branding

--- projekt_egzaminacyjny/src/app/shared/components/header/header.component.ts
+++ projekt_egzaminacyjny/src/app/shared/components/header/header.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private router: Router){}
+
+  redirectToHome(){
+    this.router.navigate(['/home']);
+  }
 }
