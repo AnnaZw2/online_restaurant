9f8121c3eafa03d69924e10a31039c63f26769bf
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2024-01-05 14:08:27 +0100

    init quiz service

---
+++ projekt_egzaminacyjny/src/app/features/services/quiz.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class QuizService {
+  // private api_url = 'http://localhost:3000/';
+  constructor(private http: HttpClient) {}
+}
