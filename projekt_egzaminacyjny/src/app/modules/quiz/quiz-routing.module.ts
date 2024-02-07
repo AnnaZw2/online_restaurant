58fa5f5c663d19e7f7039316cf6e6f22df86fd56
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2024-02-07 14:17:13 +0100

    remove unnescesary component

--- projekt_egzaminacyjny/src/app/modules/quiz/quiz-routing.module.ts
+++ projekt_egzaminacyjny/src/app/modules/quiz/quiz-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { QuizComponent } from './pages/quiz/quiz.component';
-import { QuizResultComponent } from './pages/quiz-result/quiz-result.component';
+
 
 const quizRoutes: Routes = [
   { path: 'quiz/:id', component: QuizComponent },
-  { path: 'quiz/:id/results', component: QuizResultComponent },
-  // Add more routes as needed
+
 ];
 
 @NgModule({
--- projekt_egzaminacyjny/src/app/modules/quiz/quiz.module.ts
+++ projekt_egzaminacyjny/src/app/modules/quiz/quiz.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuizComponent } from './pages/quiz/quiz.component';
 import { QuizRoutingModule } from './quiz-routing.module';
-import { QuizResultComponent } from './pages/quiz-result/quiz-result.component';
 import { SharedModule } from "../../shared/shared.module";
 import { QuizQuestionContainerComponent } from './components/quiz-question-container/quiz-question-container.component';
 import { CheckboxModule } from 'primeng/checkbox';
@@ -12,7 +11,6 @@ import { FormsModule } from '@angular/forms';
 @NgModule({
     declarations: [
         QuizComponent,
-        QuizResultComponent,
         QuizQuestionContainerComponent
     ],
     imports: [
