07260a0e6c5d7c0a651edacc6acecf80efb79998
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2024-02-08 00:44:52 +0100

    filter by dropdown

--- projekt_egzaminacyjny/src/app/modules/home/components/controls-panel/controls-panel.component.ts
+++ projekt_egzaminacyjny/src/app/modules/home/components/controls-panel/controls-panel.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
+import { CategoryEnum } from 'src/app/features/dto/category.enum';
 
 @Component({
   selector: 'app-controls-panel',
@@ -7,10 +8,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./controls-panel.component.scss']
 })
 export class ControlsPanelComponent {
+  filterByCategory: CategoryEnum | null = null;
+  categoryOptions: CategoryEnum[] = Object.values(CategoryEnum);
+  @Output() filtersApplied: EventEmitter<any> = new EventEmitter<any>();
+  @Output() filtersCleared: EventEmitter<void> = new EventEmitter<void>();
+
+  
 
   constructor(private router:Router) {
-  
    }
+   applyFilters() {
+    this.filtersApplied.emit({
+      category: this.filterByCategory
+    });
+    console.log("emitted",this.filterByCategory)
+  }
 
    navigateToCreateQuiz() {
     this.router.navigate(['/create-quiz']);
--- projekt_egzaminacyjny/src/app/modules/home/components/display-all-quizes/display-all-quizes.component.ts
+++ projekt_egzaminacyjny/src/app/modules/home/components/display-all-quizes/display-all-quizes.component.ts
@@ -2,6 +2,8 @@ import { QuizService } from '../../../../features/services/quiz/quiz.service';
 import { Component, OnInit } from '@angular/core';
 import { QuizDto } from 'src/app/features/dto/quiz.dto';
 import { Router } from '@angular/router';
+import { UserService } from 'src/app/features/services/user/user.service';
+import { CategoryEnum } from 'src/app/features/dto/category.enum';
 
 @Component({
   selector: 'app-display-all-quizes',
@@ -10,11 +12,15 @@ import { Router } from '@angular/router';
 })
 export class DisplayAllQuizesComponent implements OnInit {
   quizzes: QuizDto[] = [];
+  filteredQuizzes: QuizDto[] = [];
   currentPage = 1;
   pageSize = 10; // Number of items per page
   totalQuizzes = 0;
   first = this.currentPage * this.pageSize - this.pageSize
-  constructor(private quizService: QuizService, private router: Router) {}
+
+  selectedCategory: CategoryEnum | null = null
+  constructor(private quizService: QuizService,private userService: UserService, private router: Router) {}
+ 
 
   ngOnInit() {
     console.log('DisplayAllQuizesComponent');
@@ -29,9 +35,16 @@ export class DisplayAllQuizesComponent implements OnInit {
   }
 
   getPaginatedQuizzes(): QuizDto[] {
+    let quizzesToDisplay = this.quizzes; // By default, display all quizzes
+    
+    // Check if filters are applied
+    if (this.selectedCategory !== null) {
+      quizzesToDisplay = this.filteredQuizzes; // If filters are applied, display filtered quizzes
+    }
+    
     const startIndex = (this.currentPage - 1) * this.pageSize;
-    const endIndex = Math.min(startIndex + this.pageSize, this.totalQuizzes);
-    return this.quizzes.slice(startIndex, endIndex);
+    const endIndex = Math.min(startIndex + this.pageSize, quizzesToDisplay.length);
+    return quizzesToDisplay.slice(startIndex, endIndex); // Use filtered quizzes for pagination if filters are applied
   }
 
   onPageChange(event: any) {
@@ -47,5 +60,29 @@ export class DisplayAllQuizesComponent implements OnInit {
 
   redirectToQuiz(quizId: string) {
     this.router.navigate(['/quiz', quizId]);
+  }
+
+ 
+  isAuthentiticated(): boolean {
+    return this.userService.isAuthenticated();
+  }
+   
+  applyFilters(filterCriteria: any) {
+    console.log("filterCriteria",filterCriteria)
+    const { category } = filterCriteria;
+    this.selectedCategory = category;
+    // Apply filters here
+    this.filteredQuizzes = this.quizzes.filter((quiz) => {
+      // Implement filtering logic based on filter criteria
+      if (this.selectedCategory) {
+        return quiz.category === this.selectedCategory;
+      }
+      return true; // Return true if no filters are applied
+    });
+    console.log("filteredQuizzes",this.filteredQuizzes)
+    this.totalQuizzes = this.filteredQuizzes.length; // Update totalQuizzes with the length of filtered quizzes
+    // Reset currentPage when filters are applied
+    this.currentPage = 1;
+    this.first = 0;
   }
 }
