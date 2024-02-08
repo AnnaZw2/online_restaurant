081559982cfb16976d927c963ec20994594711d1
Anna Zwiefka <a.zwiefka.657@studms.ug.edu.pl>
2024-02-08 20:08:49 +0100

    fix eslint errors

--- projekt_egzaminacyjny/src/app/modules/home/components/controls-panel/controls-panel.component.ts
+++ projekt_egzaminacyjny/src/app/modules/home/components/controls-panel/controls-panel.component.ts
@@ -5,40 +5,34 @@ import { CategoryEnum } from 'src/app/features/dto/category.enum';
 @Component({
   selector: 'app-controls-panel',
   templateUrl: './controls-panel.component.html',
-  styleUrls: ['./controls-panel.component.scss']
+  styleUrls: ['./controls-panel.component.scss'],
 })
 export class ControlsPanelComponent {
-  filterByCategory: CategoryEnum | null = null;
-  filterByTitle: string | null = null;
-  showOnlyWithLikes = false;
-  sortBy = 'default';
-  categoryOptions: CategoryEnum[] = Object.values(CategoryEnum);
+  public filterByCategory: CategoryEnum | null = null;
+  public filterByTitle: string | null = null;
+  public showOnlyWithLikes: boolean = false;
+  public sortBy: string = 'default';
+  public categoryOptions: CategoryEnum[] = Object.values(CategoryEnum);
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  @Output() filtersApplied: EventEmitter<any> = new EventEmitter<any>();
-  @Output() filtersCleared: EventEmitter<void> = new EventEmitter<void>();
+  @Output() private filtersApplied: EventEmitter<any> = new EventEmitter<any>();
+  @Output() private filtersCleared: EventEmitter<void> =
+    new EventEmitter<void>();
 
-  
-
-  constructor(private router:Router) {
-   }
-   applyFilters() {
+  public constructor(private router: Router) {}
+  public applyFilters(): void {
     this.filtersApplied.emit({
       category: this.filterByCategory,
       title: this.filterByTitle,
       showOnlyWithLikes: this.showOnlyWithLikes,
       sortBy: this.sortBy,
     });
-console.log("sort!!!!!!")
   }
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  toggleShowOnlyWithLikes(event: any) {
-
+  public toggleShowOnlyWithLikes(event: any): void {
     this.showOnlyWithLikes = event.target?.checked || false;
-    console.log("showOnlyWithLikes",this.showOnlyWithLikes)
     this.applyFilters(); // Apply filters immediately when the checkbox state changes
   }
-   navigateToCreateQuiz() {
+  public navigateToCreateQuiz(): void {
     this.router.navigate(['/create-quiz']);
-}
-
+  }
 }
